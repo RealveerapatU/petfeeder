6c0cb882b03821054ce5ece6efa3e19f5d26d723
veerapat <veeraput.kb09834@gmail.com>
2025-06-13 12:01:44 +0700

    Add Register component with form functionality and styling

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/page/icons";
+import Registerforms from "@/components/register/register";
+export default function Home() {
+  return <Registerforms />;
+}
