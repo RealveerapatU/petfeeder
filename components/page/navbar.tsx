b4f6af68cd28f200e09b1767a8d726e29039b25e
veerapat <veeraput.kb09834@gmail.com>
2025-06-14 13:22:12 +0700

    Add user authentication handling in Navbar; display username or sign-in option

--- components/page/navbar.tsx
+++ components/page/navbar.tsx
@@ -15,7 +15,7 @@ import {
   NavbarMenu,
   NavbarMenuItem,
 } from "@heroui/react";
-import React from "react";
+import React, { useEffect } from "react";
 
 export const AcmeLogo = () => {
   return (
@@ -75,7 +75,17 @@ export const SearchIcon = ({
 };
 
 export default function App() {
+  const [username, setusername] = React.useState<string | string>("");
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  useEffect(() => {
+    const getusername = localStorage.getItem("petfeederusername");
+    if (getusername === null || getusername === undefined) {
+      setusername("undefined");
+
+      return;
+    }
+    setusername(username!);
+  }, [username]);
 
   const menuItems = [
     "Profile",
@@ -138,31 +148,61 @@ export default function App() {
           type="search"
         /> */}
         <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
-          </DropdownTrigger>
+          {username === "undefined" ? (
+            <>
+              <DropdownTrigger>
+                <Avatar
+                  isBordered
+                  as="button"
+                  className="transition-transform"
+                  color="secondary"
+                  name="Jason Hughes"
+                  size="sm"
+                  src="https://tinyurl.com/38cx6btk"
+                />
+              </DropdownTrigger>
+            </>
+          ) : (
+            <DropdownTrigger>
+              <Avatar
+                isBordered
+                as="button"
+                className="transition-transform"
+                color="secondary"
+                name="Jason Hughes"
+                size="sm"
+                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              />
+            </DropdownTrigger>
+          )}
 
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
-            </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger">
-              Log Out
+              {username == "undefined" ? (
+                <>
+                  <a href="/register">
+                    <p className="font-semibold">Sign in</p>
+                  </a>
+                </>
+              ) : (
+                <>
+                  <p className="font-semibold">Signed in as</p>
+                  <p className="font-semibold">{username}</p>
+                  <DropdownItem key="settings">My Settings</DropdownItem>
+                  <DropdownItem key="team_settings">Team Settings</DropdownItem>
+                  <DropdownItem key="analytics">Analytics</DropdownItem>
+                  <DropdownItem key="system">System</DropdownItem>
+                  <DropdownItem key="configurations">
+                    Configurations
+                  </DropdownItem>
+                  <DropdownItem key="help_and_feedback">
+                    Help & Feedback
+                  </DropdownItem>
+                  <DropdownItem key="logout" color="danger">
+                    Log Out
+                  </DropdownItem>
+                </>
+              )}
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
