6c6a514bc0cd32e7e3bc0a413bd3f6779e3f452f
veerapat <veeraput.kb09834@gmail.com>
2025-06-15 18:08:07 +0700

    Update sign-in link in navbar to redirect to the correct signin page

--- components/page/navbar.tsx
+++ components/page/navbar.tsx
@@ -180,7 +180,7 @@ export default function App() {
             <DropdownItem key="profile" className="h-14 gap-2">
               {username == "undefined" ? (
                 <>
-                  <a href="/signup">
+                  <a href="/signin">
                     <p className="font-semibold">Sign in</p>
                   </a>
                 </>
